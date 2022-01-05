50c934f160251022fba8eef8f77f15dca4631d15
Tim Underwood <timunderwood@gmail.com>
2022-01-05 08:54:58 -0800

    Change method for stripping trailing whitespace to be more compatible with older version of SBT
    For an input of "2.10.6 2.11.11 2.12.10 2.13.1" I was only getting back ["2.10.6", "2.11.11", "2.12.10"] on older version of SBT

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,9 +4,9 @@ set -e
 set -x
 
 # e.g.) 2.12.14 2.13.7 3.1.0
-versions=$(sbt -Dsbt.supershell=false --error "print crossScalaVersions" | sed -r 's/\* (.+)/\1/')
+versions=$(sbt -Dsbt.supershell=false --error "print crossScalaVersions" | sed -r 's/\* (.+)/\1/' | sed 's/ *$//g')
 
 # e.g.) ["2.12.14", "2.13.7", "3.1.0"]
-scala_versions="[\"$(echo "$versions" | sed '$d' | xargs | sed -e 's/ /", "/g')\"]"
+scala_versions="[\"$(echo "$versions" | xargs | sed -r 's/ +$//g' | sed 's/ /", "/g')\"]"
 
 echo "::set-output name=scala_versions::$scala_versions"
