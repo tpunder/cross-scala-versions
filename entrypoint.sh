4e066be8812126d0f34a56f90bd10c1de15e8361
Tim Underwood <timunderwood@gmail.com>
2022-01-06 11:35:22 -0800

    Add support for crossSbtVersions

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,10 +3,22 @@
 set -e
 set -x
 
-# e.g.) 2.12.14 2.13.7 3.1.0
-versions=$(sbt -Dsbt.supershell=false --error "print crossScalaVersions" | sed -r 's/\* (.+)/\1/')
+get_versions()
+{
+  sbt_key=$1
+  output_name=$2
 
-# e.g.) ["2.12.14", "2.13.7", "3.1.0"]
-scala_versions="[\"$(echo "$versions" | sed '$d' | xargs | sed -e 's/ /", "/g')\"]"
+  # e.g.) 2.12.14 2.13.7 3.1.0
+  versions=$(sbt -Dsbt.supershell=false --error "print $sbt_key" | sed -r 's/\* (.+)/\1/')
 
-echo "::set-output name=scala_versions::$scala_versions"
+  # e.g.) ["2.12.14", "2.13.7", "3.1.0"]
+  formatted_versions="[\"$(echo "$versions" | sed '$d' | xargs | sed -e 's/ /", "/g')\"]"
+
+  echo "::set-output name=$output_name::$formatted_versions"
+}
+
+# Scala Versions (via crossScalaVersions)
+get_versions crossScalaVersions scala_versions
+
+# SBT Versions (via crossSbtVersions)
+get_versions crossSbtVersions sbt_versions
